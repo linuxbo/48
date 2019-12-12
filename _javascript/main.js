b53b48fbd21d9f5c7f4841c7ddde2cbbf561a882
Linus Chan <linuschan@Linuss-MacBook-Pro.local>
2019-12-11 17:17:51 -0800

    changes

--- _javascript/main.js
+++ _javascript/main.js
@@ -5,7 +5,8 @@ function passWord() {
    if (!pass1)
    history.go(-1);
    if (pass1.toLowerCase() == "48minutes") {
-       window.open("/48/48192231/Rina2131.html")
+       window.open("/48/48192231/Rina2131.html",'_self')
+       return"";
    }
    
    testV+=1;
