4d077f884672505d00bc454b76b250294e9559bd
Linus Chan <linuschan@Linuss-MacBook-Pro.local>
2019-12-11 17:08:07 -0800

    changes

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+function passWord() {
+   var testV = 1;
+   var pass1 = prompt('Password',' ');
+   while (testV < 3) {
+   if (!pass1)
+   history.go(-1);
+   if (pass1.toLowerCase() == "48minutes") {
+       window.open("/48/48192231/Rina2131.html")
+   }
+   
+   testV+=1;
+   var pass1 =
+   prompt('','Password');
+   }
+   if (pass1.toLowerCase()!="password" & testV ==3)
+   history.go(-1);
+   return " ";
+   }
